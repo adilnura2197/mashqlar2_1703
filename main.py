0e989da2f9923cef74aa5c967eb37eec2311802c
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-03-16 16:14:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#1
+a = list((1, 2, 3, 4))
+print(a)
+
+
+#2
+a = [10, 20, 30]
+print(a)
+
+a.append(40)
+print(a)
+
+
+#3
+davlatlar = ["Uzbekistan", "Kazakhstan"]
+print(davlatlar)
+
+davlatlar.append("Kyrgyzstan")
+print(davlatlar)
+
+
+#4
+a = [7, 8, 9]
+print(a)
+
+print(len(a))
+
+
+#5
+a = [1, 2, 3, 4]
+
+for i in a:
+    print(i)
